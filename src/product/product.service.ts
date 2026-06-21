89975b6b66f4208ea63b66fee52a55dd301e7b69
LeTheKhoaa <24103354@st.phenikaa-uni.edu.vn>
2026-06-21 11:06:40 +0700

    Implement ProductService with CRUD methods

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,1 +1,23 @@
+export class ProductService {
 
+create(product: any) {
+  return "Create Product";
+}
+
+findAll() {
+  return "Get All Product";
+}
+
+findOne(id: number) {
+  return "Get Product ${id}";
+}
+
+update(id: number, product: any) {
+  return "Update Product ${id}";
+}
+
+remove(id: number) {
+  return "Delete Product ${id}";
+}
+
+}
